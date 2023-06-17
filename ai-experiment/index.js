d8fd4acbfe0526d5cb822e22c4378da21a8076bc
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2023-06-17 16:14:52 -0400

    gpt 3.5

--- ai-experiment/index.js
+++ ai-experiment/index.js
@@ -10,10 +10,11 @@ const openai = new OpenAIApi(configuration);
 
 
 async function runCompletion () {
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Continue the below interactive story."
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "system", "content": "Insert backstory context here."}, {"role": "user", "content": "Insert user message here."}],
+        max_tokens: 256
     });
-    console.log(completion.data.choices[0].text);
+    console.log(completion.data.choices[0].message.content);
 }
 runCompletion();
